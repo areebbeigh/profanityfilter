4aeefa48d2ee42686599ad0c0e163ba4eedd1a95
Areeb Beigh <areebbeigh@gmail.com>
2018-12-03 17:17:51 +0530

    Cleaning up

--- .travis/generate-docs.sh
+++ .travis/generate-docs.sh
@@ -4,21 +4,5 @@ make_doc_files() {
     make html
 }
 
-# push_github_pages() {
-#     cd ../github-pages
-#     git config --global user.email "travis@travis-ci.org"
-#     git config --global user.name "Travis CI"
-
-#     git add ./*
-#     git commit --message "[ci skip] Travis build: $TRAVIS_BUILD_NUMBER"
-
-#     git remote remove origin
-#     git remote add origin https://${GH_TOKEN}@github.com/areebbeigh/profanityfilter.git
-
-#     git push origin gh-pages
-# }
-
 pip install sphinx
 make_doc_files
-
-# push_github_pages
