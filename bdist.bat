fa1dce4f6dbf9d2dda28a4619f1d63824a39de6e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-03 11:23:33 +0000

    Deploy areebbeigh/profanityfilter to github.com/areebbeigh/profanityfilter.git:gh-pages

--- bdist.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Probably specific to only my system py - python2 ; python - python3
-python setup.py sdist bdist_wheel
--- profanityfilter/__init__.py
+++
@@ -1,4 +0,0 @@
-from .profanityfilter import *
-from .console import main
-__version__ = "1.0"
-__author__ = "Areeb Beigh <areebbeigh@gmail.com>"
