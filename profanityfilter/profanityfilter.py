4ecf7f92e2f07906f0ff5b2989ec9fd44e50dfe1
Areeb Beigh (Euphinx) <areebbeigh@gmail.com>
2019-02-20 12:05:44 -0500

    remove word(s) from custom or extra censor list
    Co-Authored-By: RicoViking9000 <38385704+RicoViking9000@users.noreply.github.com>

--- profanityfilter/profanityfilter.py
+++ profanityfilter/profanityfilter.py
@@ -68,7 +68,7 @@ class ProfanityFilter:
             else:
                 self._censor_list.remove(word)
                     
-     def remove_word_list(self, word_list, anywhere=True):
+     def remove_words(self, word_list, anywhere=True):
         """
         Remove given list of words from censor list.
 
