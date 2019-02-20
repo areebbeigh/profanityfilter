e59385cf0e2cd13ba872b95c099a127a0b82cd65
Areeb Beigh (Euphinx) <areebbeigh@gmail.com>
2019-02-20 11:58:17 -0500

    remove word(s) from custom or extra censor list
    Co-Authored-By: RicoViking9000 <38385704+RicoViking9000@users.noreply.github.com>

--- profanityfilter/profanityfilter.py
+++ profanityfilter/profanityfilter.py
@@ -62,7 +62,7 @@ class ProfanityFilter:
         if word in self._extra_censor_list and anywhere:
             self._extra_censor_list.remove(word)
         else:
-            if self._custom_censor_list: #NEW - now supports word removal from custom list, if applicable - saves defining new lists to remove words from custom list
+            if self._custom_censor_list:
                 self._custom_censor_list.remove(word)
             else:
                 self._censor_list.remove(word)
