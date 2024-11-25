605eb0c7dd2bbb1af81823ae3e1194703019724e
William Dutton <william.dutton@qld.gov.au>
2024-11-26 06:58:56 +1000

    chore: Add CICD to repo

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from setuptools import setup, find_packages
-
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
 from os import path
 
 here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='profanityfilter',
     version='2.0.6',
     description='A universal Python library for detecting and/or filtering profane words.',
-    long_description='For more details visit https://github.com/areebbeigh/profanityfilter',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/areebbeigh/profanityfilter',
     author='Areeb Beigh',
     author_email='areebbeigh@gmail.com',
@@ -24,9 +30,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
