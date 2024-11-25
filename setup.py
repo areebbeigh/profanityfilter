eef65354db21921782b0e394822a046c257dd626
William Dutton <william.dutton@qld.gov.au>
2024-11-26 07:40:57 +1000

    chore: increment version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='profanityfilter',
-    version='2.0.6',
+    version='2.1.0',
     description='A universal Python library for detecting and/or filtering profane words.',
     long_description=long_description,
     long_description_content_type='text/markdown',
